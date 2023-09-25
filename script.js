56c26b85224ad87b8922bf4a6e25178ed1ce2b27
Barberopvt <127975354+Barberopvt@users.noreply.github.com>
2023-09-26 00:24:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,60 +9,39 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var book = document.getElementById("bookbtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  window.open("https://wa.me/+919142004623?text=Hello SilkWave I have an enquiry!")
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+book.onclick = function() {
+    window.open("https://wa.me/+919142004623?text=Hello SilkWave I have an enquiry!")
   }
-}
 
-    const text = "This is a typewriter effect example.";
-    const delay = 100;
+
+    const textArr = ["Social media management", "Digital Marketing and Branding", "Website Designing and Development"];
+    const delay = 2000;
     const container = document.getElementById("typewriter-text");
     let index = 0;
-    let i = text.length-1;
 
+    let next = 0;
     function type() {
-        if (index < text.length) {
-            container.textContent += text.charAt(index);
-            index++;
+            container.textContent = textArr[index%3];
+            index +=1;
             setTimeout(type, delay);
         }
-        else if(index === text.length){
-            i = text.length-1;
-            unType(); 
-        }
-    }
-    function unType() {
-        if (i > 0) {
-            container.textContent = text.slice(0,i);
-            i--;
-            console.log(text);
-            setTimeout(unType, delay);
-            
-        }
-        else if(i == 0){
-            index = 0;
-            type();
-        }
 
-    }
 
-    // Start the typing animation
+
+
     type();
     
 });
+
+
+
